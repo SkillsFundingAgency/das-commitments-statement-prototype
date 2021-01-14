86333b93c6545dc37b5b4122b527e723cd916611
chrishewison1983 <chris@chewitdesign.co.uk>
2021-01-14 11:03:53 +0000

    UR updates to the prototype

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -178,3 +178,55 @@ $("#confirm-and-sign").on("click", function (e) {
 // });
 
 // 5
+
+
+// Side nav
+// let mainNavLinks = document.querySelectorAll("nav ul li a");
+// let mainSections = document.querySelectorAll("main section");
+//
+// let lastId;
+// let cur = [];
+//
+// // This should probably be throttled.
+// // Especially because it triggers during smooth scrolling.
+// // https://lodash.com/docs/4.17.10#throttle
+// // You could do like...
+// // window.addEventListener("scroll", () => {
+// //    _.throttle(doThatStuff, 100);
+// // });
+// // Only not doing it here to keep this Pen dependency-free.
+//
+// window.addEventListener("scroll", event => {
+//      let fromTop = window.scrollY;
+//
+//      mainNavLinks.forEach(link => {
+//           let section = document.querySelector(link.hash);
+//
+//           if (
+//                section.offsetTop <= fromTop &&
+//                section.offsetTop + section.offsetHeight > fromTop
+//           ) {
+//           link.classList.add("current");
+//           } else {
+//                link.classList.remove("current");
+//           }
+//      });
+// });
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+     if (window.pageYOffset >= sticky) {
+          navbar.classList.add("sticky");
+     } else {
+          navbar.classList.remove("sticky");
+     }
+}
