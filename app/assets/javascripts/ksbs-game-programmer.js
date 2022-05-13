b50bbbac072af2bb65a2dccf31bf97c6cc745778
ash-h <a.henry@kuumba.co.uk>
2022-05-13 15:35:00 +0100

    Track progress

---
+++ app/assets/javascripts/ksbs-game-programmer.js
@@ -0,0 +1,310 @@
+
+const behaviourList = [
+    {
+        "id": "374c7f75-5f70-494d-9f03-7d595dae7bee",
+        "detail": "Reliable, objective and capable of independent working."
+    },
+    {
+        "id": "41e13048-a716-4789-b92c-f453adbdbb8c",
+        "detail": "Initiative and personal responsibility to overcome challenges and take ownership for project solutions."
+    },
+    {
+        "id": "2aa61c78-f51b-49f2-90c6-05b0ec6c99fb",
+        "detail": "Respect for other disciplines and an understanding of the role of diverse experiences and backgrounds in a successful creative process."
+    },
+    {
+        "id": "6ca51240-7a89-40a2-8e6a-5ade9b9d9142",
+        "detail": "Commitment to continuous professional development; maintaining their knowledge and skills in relation to technology developments, and sharing best practice in their organisation around all aspects of game development. "
+    },
+    {
+        "id": "2f7d13f3-12f4-4fca-a932-f7aa3651713a",
+        "detail": "Maintains awareness of trends and innovations in the subject area, utilizing a range of academic literature, online sources, community interaction and conference attendance."
+    },
+    {
+        "id": "1b676f99-9d71-4b22-9931-da8278d4e191",
+        "detail": "Acts with integrity with respect to ethical, legal and regulatory ensuring the protection of personal data, safety and security. "
+    },
+    {
+        "id": "19bccbdd-e259-468f-a33a-91bef2d7f683",
+        "detail": "A strong work ethic and commitment in order to meet the standards required."
+    }
+];
+const skillList = [
+    {
+        "id": "65c82315-2326-4976-937d-ba18e41de80c",
+        "detail": "Program interactive, real-time applications for gaming platforms using an industry-standard programming language, incorporating APIs, tools, engines or frameworks appropriate to employer requirements. "
+    },
+    {
+        "id": "c99e0c71-8d53-4a4f-8165-9805abee9758",
+        "detail": "Implement and adapt contemporary real-time algorithms in two and three-dimensional games. "
+    },
+    {
+        "id": "e3214ea4-27f2-458d-bc95-eb932bdb196d",
+        "detail": "Use profiling tools and techniques to achieve, monitor and maintain an acceptable real-time framerate for an interactive game."
+    },
+    {
+        "id": "38d6fd7d-e8d7-4aa3-80ce-b98056b6d57b",
+        "detail": "Track memory usage and identify opportunities for reducing requirements."
+    },
+    {
+        "id": "4f4af709-7caf-4b78-862d-94c54d0af9f0",
+        "detail": "Write code informed by the characteristics of modern hardware platforms (e.g. shader programming, multi-threading). "
+    },
+    {
+        "id": "cded5ac4-0220-4fc5-bd00-fa940ebb9fcf",
+        "detail": "Use debugging tools and automated testing systems to develop robust code bases."
+    },
+    {
+        "id": "92a3b92c-2728-4267-81a0-7cb81d7d7d10",
+        "detail": "Use continuous integration workflow within the deployment lifecycle as part of a multi-disciplinary software team."
+    },
+    {
+        "id": "d8547692-891c-4259-9d9d-bd7cf3c5a2d4",
+        "detail": "Write robust, well-tested, maintainable code which is easy to adapt to changing requirements."
+    },
+    {
+        "id": "a7bf8d5b-0f62-4ac9-a9ad-4ddd11d0cb01",
+        "detail": "Use an industry-standard version control system."
+    },
+    {
+        "id": "b783b8cd-e3e8-4369-88a9-c71fecdcc02f",
+        "detail": "Use an industry-standard project management system from the perspective of a developer."
+    },
+    {
+        "id": "0b9adcd5-fd4a-4697-9c33-fb8f3b263a13",
+        "detail": "Adapt or extend existing tool chains to support new features and/or optimise workflows."
+    },
+    {
+        "id": "a78ade4a-fafc-4b4c-885f-baa0e5e6f5ec",
+        "detail": "Apply industry-standard development methodologies within day-to-day working practice."
+    },
+    {
+        "id": "e62e8b97-e558-400b-a4bb-4a5912383552",
+        "detail": "Manage complex relationships with diverse stakeholders and communicate information effectively to different audiences."
+    },
+    {
+        "id": "ec740bed-3787-43d0-bc90-69091b12d1e5",
+        "detail": "Provide technical leadership and direction with respect to the workflow of other team members."
+    },
+    {
+        "id": "43f30e11-6f3f-4b6a-9661-9de85e48e2ad",
+        "detail": "Research, document and articulate the opportunities and threats presented by new industry technologies."
+    },
+    {
+        "id": "257fa37b-cabe-48a5-8b01-080b4901a39e",
+        "detail": "Follow studio coding best-practices and participate in keeping them relevant and up to date."
+    },
+    {
+        "id": "f4377b6c-ca39-444a-911b-19fc935655da",
+        "detail": "Give and receive feedback in code reviews in an objective and professional manner."
+    },
+    {
+        "id": "35c9d855-6aab-40e9-b624-7f3ec54b0cf1",
+        "detail": "Develop games and/or prototypes using an industry-standard or in-house game engine."
+    },
+    {
+        "id": "918f1192-b44c-4e28-a281-ea15cab18f51",
+        "detail": "Make justified choices about the implementation of different features and tools and their effect on the overall workload of the team."
+    },
+    {
+        "id": "942aa60d-63d3-4576-b505-4f10c388cd15",
+        "detail": "Write software which contributes to the player experience while balancing the extensibility and performance requirements for an evolving game design."
+    },
+    {
+        "id": "5172de8e-7282-4dc4-b351-47237cf8cc62",
+        "detail": "Work as part of interdisciplinary teams, collaborating closely with disciplines outside programming, for example artists, game designers and audio engineers."
+    },
+    {
+        "id": "e57192dc-fc65-432b-8e2b-fea27c5d1553",
+        "detail": "Create innovative game mechanics for which solutions are unknown."
+    },
+    {
+        "id": "89e3ce1b-7580-4f78-8639-6fdc8aa3d433",
+        "detail": "Develop reusable technologies targeting specific hardware architectures or gaming platforms."
+    },
+    {
+        "id": "6b4f2622-8586-4629-9764-1af2af70a954",
+        "detail": "Make justified decisions about the implementation of different features and their effect on quality and workload for their technology’s user base."
+    },
+    {
+        "id": "5f656936-12c5-4fb0-8d74-25b576592c30",
+        "detail": "Work as part of a user-focused product team, incorporating multi-disciplinary input from outside of the team, for example from game software programmers, artists, game designers and audio engineers."
+    },
+    {
+        "id": "b37ab6be-5961-439b-99d1-246e13139912",
+        "detail": "Communicate and evangelise technology solutions to promote engagement and uptake among the user-base."
+    },
+    {
+        "id": "d1657bb9-60a0-4c2b-b23d-462672a17c9a",
+        "detail": "Profile and optimise code created by their technology users."
+    }
+];
+const knowledgeList = [
+    {
+        "id": "d762b5fb-7705-4de4-a862-df420d8ef692",
+        "detail": "How to approach the development of interactive, real-time applications for gaming platforms, including an awareness of industry-standard programming languages, application programming interfaces (APIs), tools, engines and frameworks."
+    },
+    {
+        "id": "2091933a-12f1-4b40-a7c7-5214b76dc6fd",
+        "detail": "The syntax and structure of an industry-standard programming language (above and beyond visual programming languages) used for the development of games (for example C++, C#)."
+    },
+    {
+        "id": "55dc6160-5791-4336-8645-d80f0552e308",
+        "detail": "The fundamental graphical and mathematical principles that underpin the operation of real-time graphics in two and three-dimensions."
+    },
+    {
+        "id": "6bb4cfc7-1866-4cdf-8c2c-da42cc2433ee",
+        "detail": "The characteristics of modern hardware platforms and how they support the efficient function of interactive, real-time graphical applications."
+    },
+    {
+        "id": "71046fb6-1883-40c5-8763-55800eedf67f",
+        "detail": "Approaches to balancing quality and performance requirements to achieve, monitor and maintain acceptable frame rates and memory footprints for a real-time interactive application."
+    },
+    {
+        "id": "9f46b02f-394f-45e5-ad25-5220cab468ef",
+        "detail": "How to use tools to identify and optimise performance bottlenecks in real-time applications."
+    },
+    {
+        "id": "02dba563-27c6-4cb2-a48b-cfb62d2241df",
+        "detail": "The role of  debugging tools, crash reports, automated testing and continuous integration workflows in creating robust software."
+    },
+    {
+        "id": "23ff597b-4c2c-4500-bee5-056e72055502",
+        "detail": "The role of staged deployment, monitoring and analytics in releasing, tracking and refining games."
+    },
+    {
+        "id": "f0575c92-078e-48e2-8d2e-f8305a6c86b8",
+        "detail": "Common principles of good software design applied in the games industry including contrasting approaches and priorities (e.g. object-oriented vs. data-oriented)"
+    },
+    {
+        "id": "c4caf042-9114-4ec4-ae9b-f3593d102c19",
+        "detail": "How a complete asset pipeline for a game operates, including the technical requirements, processing stages and tools involved in bringing assets into the game. "
+    },
+    {
+        "id": "84b9439c-f526-434c-b0a8-30c84dabf7c0",
+        "detail": "How to use version control and project management tools to plan and coordinate the delivery of development tasks."
+    },
+    {
+        "id": "31e2ff78-d57d-4f2a-9729-343f55468cc0",
+        "detail": "Common development methodologies and how they are applied in game development. "
+    },
+    {
+        "id": "52a22dd0-7f19-4060-a4eb-91051f4bb404",
+        "detail": "The broad range of roles involved in the game development process, and the different strengths and perspectives that multi-disciplinary teams bring to the creative process."
+    },
+    {
+        "id": "8b5d91ac-ecc6-4fe4-b398-07c51c25e78b",
+        "detail": "Where to find information on the latest technological innovations for the games industry."
+    },
+    {
+        "id": "a005c970-ad86-4591-9812-205ddf474260",
+        "detail": "The role of rapid prototyping and agile approaches in innovation."
+    },
+    {
+        "id": "7068b2c8-b78b-4743-9b3f-a9218135285a",
+        "detail": "The organisation’s standards with respect to coding, documentation and issue tracking, and how they relate to wider practice in the software industries."
+    },
+    {
+        "id": "b62aaa7d-8afe-4868-9e40-04d4a4f0b3da",
+        "detail": "Publisher’s technical requirements for target platforms, where to obtain them and the tools and systems available to support developers to meet those requirements."
+    },
+    {
+        "id": "d365d532-351f-4f1c-abf0-ffa275d65b85",
+        "detail": "The business stakeholders in a project and how multi-disciplinary development teams can generate value within the context of different business models."
+    },
+    {
+        "id": "4f0db6d3-7f2e-4598-b1fd-a5614a5da6b2",
+        "detail": "Relevant data protection laws including GDPR. "
+    },
+    {
+        "id": "7d967390-8f26-4a32-b739-6d4f9a3b5f9f",
+        "detail": "Security approaches to prevent products being compromised, and everyday good practice in security including password policies, phishing and use of VPNs."
+    },
+    {
+        "id": "3c9b90db-74d8-4e1b-9318-bdfde4bf4498",
+        "detail": "The relative merits of different game engines, third-party frameworks and tools, and when to use them to speed up the development process."
+    },
+    {
+        "id": "89d19d43-ef25-4f38-b031-9a20c10a811a",
+        "detail": "How to balance the requirements and availability of team resources (for example staff time, software licencing) with respect to the engineering and maintenance of a game’s asset pipeline."
+    },
+    {
+        "id": "d9568bbb-7179-4fa4-8cfa-6fd0e7930eb8",
+        "detail": "The range of different disciplines involved in the development process and their typical skillsets and expectations in terms of technologies, tools and asset formats."
+    },
+    {
+        "id": "48a2046c-0e8d-43f8-82f7-500a144a8541",
+        "detail": "The specialist operation of a specific hardware architecture or gaming platform and how to engineer efficient solutions which target its specific capabilities."
+    },
+    {
+        "id": "368fb811-0314-4b2d-9124-9a4d74874f10",
+        "detail": "How to balance the requirements and availability of team resources (staff time, software licencing) with respect to providing the maximum benefit to their users."
+    },
+    {
+        "id": "13925e16-66d8-474e-bd92-dc889ebae5c5",
+        "detail": "How to use externally facing support portals and project tracking tools in order to effectively track and document technologies for sharing with a wide user-base."
+    }
+];
+
+console.log("ee");
+document.addEventListener("DOMContentLoaded", function(){
+    const bList = document.getElementsByClassName('ksb-b-list')[0];
+    const sList = document.getElementsByClassName('ksb-s-list')[0];
+    const kList = document.getElementsByClassName('ksb-k-list')[0];
+
+    function createList(target, data, ksb) {
+        if (target) {
+            data.forEach((item, index) => {
+                console.log(item)
+                target.insertAdjacentHTML("beforeend",
+                    `<div>
+                          <p id=${index+1}>
+                            <strong>${ksb}${index+1}</strong>: ${item.detail} 
+                        </p>
+                        <div class="govuk-form-group govuk-!-margin-top-4">
+                            <fieldset class="govuk-fieldset" aria-describedby="changed-name-hint">
+                                <legend class="govuk-fieldset__legend govuk-fieldset__legend--s">
+                                        <span class="govuk-fieldset__heading">
+                                            Apprentice's progress for this KSB
+                                        </span>
+                                </legend>
+                                <div class="govuk-radios govuk-radios--inline govuk-radios--small" data-module="govuk-radios">
+                                    <div class="govuk-radios__item">
+                                        <input class="govuk-radios__input" id=${ksb}${index+1} name=${item.detail} type="radio" checked="checked" value="Not started">
+                                        <label class="govuk-label govuk-radios__label" for=${item.detail}>
+                                            Not started
+                                        </label>
+                                    </div>
+                                    <div class="govuk-radios__item">
+                                        <input class="govuk-radios__input" id=${ksb}${index+2} name=${item.detail} type="radio" value="Not on track">
+                                        <label class="govuk-label govuk-radios__label" for=${item.detail}>
+                                            Not on track
+                                        </label>
+                                    </div>
+                                    <div class="govuk-radios__item">
+                                        <input class="govuk-radios__input" id=${ksb}${item.id} name=${item.detail} type="radio" value="On track">
+                                        <label class="govuk-label govuk-radios__label" for=${item.detail}>
+                                            On track
+                                        </label>
+                                    </div>
+                                      <div class="govuk-radios__item">
+                                        <input class="govuk-radios__input" id=${ksb}${item.id} name=${item.detail} type="radio" value="Ready for EPA">
+                                        <label class="govuk-label govuk-radios__label" for=${item.detail}>
+                                            Ready for EPA
+                                        </label>
+                                    </div>
+                                </div>
+                
+                            </fieldset>
+                            <hr>
+                        </div>
+                      </div>`)
+
+            });
+        }
+    }
+
+    createList(bList,behaviourList, "B")
+    createList(sList,skillList, "S")
+    createList(kList,knowledgeList, "K")
+});
