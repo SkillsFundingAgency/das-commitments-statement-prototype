a2d1beb0da218a7b23c011e5a2132d568aff55ae
Martin Coulthurst <mc@lightness.co.uk>
2022-10-07 14:40:29 +0100

    Create a second version of the navigation styles to render the banners in full width without js

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'My Apprenticeship',
+  serviceName: 'My apprenticeship',
 
   // Default port that prototype runs on
   port: '3000',
